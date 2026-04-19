89de670833b525d7c5b834ee66720dc474876c41
rohen-13 <231649092+rohen-13@users.noreply.github.com>
2026-04-19 18:02:24 +0300

    fixed

--- src/test/BookingDAOTest.java
+++ src/test/BookingDAOTest.java
@@ -4,7 +4,7 @@ import dao.BookingDAO;
 import exception.InvalidBookingException;
 import model.Booking;
 import org.junit.After;
-import org.junit.AfterClass;
+import org.testng.annotations.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
